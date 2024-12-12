219b36e608b4f464c2c8a3f92465e472ce8387f3
Abhinav Gaba <abhinav.gaba@intel.com>
2024-12-12 02:57:39 -0800

    Use lualine-fancy based lualine config.

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,59 +1,106 @@
 local M = {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    "AndreM222/copilot-lualine",
+    "nvim-tree/nvim-web-devicons",
+    -- "AndreM222/copilot-lualine",
+    "meuter/lualine-so-fancy.nvim",
   },
 }
 
+--------------------------------------------------------------------------------
+-- Custom functions from lualine wiki
+--------------------------------------------------------------------------------
+-- Currently active keymap
+local function keymap()
+  if vim.opt.iminsert:get() > 0 and vim.b.keymap_name then
+    return "⌨ " .. vim.b.keymap_name
+  end
+  return ""
+end
+
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ""
+    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+      return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+    end
+    return str
+  end
+end
+
+--------------------------------------------------------------------------------
+-- Lualine-fancy config
+--------------------------------------------------------------------------------
 function M.config()
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    -- component_separators = { left = '', right = ''},
-    -- section_separators = { left = '', right = ''},
-    -- component_separators = { left = '', right = ''},
-    -- section_separators = { left = '', right = ''},
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-  -- section_separators = { left = '', right = '' },
-  -- component_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+  local lualine = require "lualine"
+
+  lualine.setup {
+    options = {
+      theme = "ayu_mirage",
+      icons_enabled = true,
+      component_separators = { left = "│", right = "│" },
+      section_separators = { left = "", right = "" },
+      -- component_separators = { left = '', right = ''},
+      -- section_separators = { left = '', right = ''},
+      -- component_separators = { left = '', right = ''},
+      -- section_separators = { left = '', right = ''},
+      -- component_separators = { left = "", right = "" },
+      -- section_separators = { left = "", right = "" },
+      -- section_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = '' },
+      globalstatus = true,
+      refresh = {
+        statusline = 100,
+      },
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 100,
-      tabline = 100,
-      winbar = 100,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'searchcount', 'selectioncount'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = { "quickfix", "man", "mason", "fugitive", "trouble", "lazy" },
-}
+    sections = {
+      lualine_a = {
+        { "fancy_mode", width = 1 },
+      },
+      lualine_b = {
+        { "fancy_branch", fmt = trunc(0, 0, 120, true) },
+        { "fancy_diff", fmt = trunc(0, 0, 25, true) },
+      },
+      lualine_c = {
+        {
+          "filename",
+          file_status = true, -- displays file status (readonly status, modified status)
+          path = 3, -- 3: Absolute path, with tilde as the home directory
+          shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          symbols = {
+            modified = "✏️", -- eText to show when the file is modified.
+            readonly = "🔒", -- Text to show when the file is non-modifiable or readonly.
+            newfile = " ",
+            unnamed = "",
+          },
+          fmt = trunc(30, 40, 30, false),
+        },
+        -- { "fancy_cwd", substitute_home = true },
+      },
+      lualine_x = {
+        { "fancy_macro", fmt = trunc(0, 0, 120, true) },
+        { keymap, fmt = trunc(0, 0, 120, true) },
+        { "fancy_diagnostics", fmt = trunc(0, 0, 50, true) },
+        { "fancy_searchcount", fmt = trunc(0, 0, 50, true) },
+        { "selectioncount", fmt = trunc(0, 0, 50, true) },
+        { "fancy_location" },
+      },
+      lualine_y = {
+        { "fancy_filetype", ts_icon = "", fmt = trunc(0, 0, 60, true) },
+      },
+      lualine_z = {
+        { "fancy_lsp_servers", fmt = trunc(0, 0, 120, true) },
+      },
+    },
+    extensions = { "quickfix", "man", "mason", "fugitive", "trouble", "lazy" },
+  }
 end
 
 return M
