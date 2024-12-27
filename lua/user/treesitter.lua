c599ff66c0849b06ed0a9d5c613a55c643508209
Abhinav Gaba <abhinav.gaba@intel.com>
2024-12-27 13:02:29 -0800

    Use vertical gitsigns diff for '<leader>gdh'.

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,20 +4,20 @@ local M = {
   build = ":TSUpdate",
 }
 
--- function M.disableNvimTS(lang, buf)
+-- function M.disableNvimTS(_, buf)
 --   local buf_name = vim.api.nvim_buf_get_name(buf)
 --
 --   -- Don't run on buffers open via :Gitsigns diffthis etc.
---   if string.find(buf_name, "gitsigns:") or string.find(buf_name, "diffview:") then
+--   if string.find(buf_name, "gitsigns:") or string.find(buf_name, "diffview:") or string.find(buf_name, "fugitive:") then
 --     return true
 --   end
 --
 --   -- Disable slow treesitter highlight for large files
---   local max_filesize = 2000 * 1024 -- 2MB
---   local ok, stats = pcall(vim.loop.fs_stat, buf_name)
---   if ok and stats and stats.size > max_filesize then
---     return true
---   end
+--   -- local max_filesize = 2000 * 1024 -- 2MB
+--   -- local ok, stats = pcall(vim.loop.fs_stat, buf_name)
+--   -- if ok and stats and stats.size > max_filesize then
+--   --   return true
+--   -- end
 -- end
 
 function M.config()
