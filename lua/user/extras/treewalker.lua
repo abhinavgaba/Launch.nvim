5b3215d769b878c1837b2237626eee1ce50f1f05
Abhinav Gaba <abhinav.gaba@intel.com>
2025-01-11 00:20:48 -0800

    NFC change - use M instead of A for alt.

--- lua/user/extras/treewalker.lua
+++ lua/user/extras/treewalker.lua
@@ -11,16 +11,16 @@ local function set_keymaps()
       local parsers = require("nvim-treesitter.parsers")
       if parsers.has_parser() then
         -- movement
-        vim.keymap.set({ "n", "v" }, "<A-k>", "<cmd>Treewalker Up<cr>", { silent = true, desc = "Treesitter Walk Up"})
-        vim.keymap.set({ "n", "v" }, "<A-j>", "<cmd>Treewalker Down<cr>", { silent = true, desc = "Treesitter Walk Down" })
-        vim.keymap.set({ "n", "v" }, "<A-l>", "<cmd>Treewalker Right<cr>", { silent = true, desc = "Treesitter Walk Right" })
-        vim.keymap.set({ "n", "v" }, "<A-h>", "<cmd>Treewalker Left<cr>", { silent = true, desc = "Treesitter Walk Left" })
+        vim.keymap.set({ "n", "v" }, "<M-k>", "<cmd>Treewalker Up<cr>", { silent = true, desc = "Treesitter Walk Up"})
+        vim.keymap.set({ "n", "v" }, "<M-j>", "<cmd>Treewalker Down<cr>", { silent = true, desc = "Treesitter Walk Down" })
+        vim.keymap.set({ "n", "v" }, "<M-l>", "<cmd>Treewalker Right<cr>", { silent = true, desc = "Treesitter Walk Right" })
+        vim.keymap.set({ "n", "v" }, "<M-h>", "<cmd>Treewalker Left<cr>", { silent = true, desc = "Treesitter Walk Left" })
 
         -- swapping
-        vim.keymap.set("n", "<A-S-j>", "<cmd>Treewalker SwapDown<cr>", { silent = true, desc = "Treesitter Swap Down" })
-        vim.keymap.set("n", "<A-S-k>", "<cmd>Treewalker SwapUp<cr>", { silent = true, desc = "Treesitter Swap Up" })
-        vim.keymap.set("n", "<A-S-l>", "<cmd>Treewalker SwapRight<CR>", { silent = true, desc = "Treesitter Swap Right" })
-        vim.keymap.set("n", "<A-S-h>", "<cmd>Treewalker SwapLeft<CR>", { silent = true, desc = "Treesitter Swap Left" })
+        vim.keymap.set("n", "<M-S-j>", "<cmd>Treewalker SwapDown<cr>", { silent = true, desc = "Treesitter Swap Down" })
+        vim.keymap.set("n", "<M-S-k>", "<cmd>Treewalker SwapUp<cr>", { silent = true, desc = "Treesitter Swap Up" })
+        vim.keymap.set("n", "<M-S-l>", "<cmd>Treewalker SwapRight<CR>", { silent = true, desc = "Treesitter Swap Right" })
+        vim.keymap.set("n", "<M-S-h>", "<cmd>Treewalker SwapLeft<CR>", { silent = true, desc = "Treesitter Swap Left" })
       end
     end,
   })
